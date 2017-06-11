63ef49e2f0c38fdd4b18a344ef2d7b938ecab7cc
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2017-06-11 22:11:21 +0200

    make old symfony compatible with new phpunit

---
+++ Tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+// fix old symfony WebTest
+$hasNewPhpUnit = class_exists('\PHPUnit\Framework\TestCase');
+$hasOldPhpUnit = class_exists('\PHPUnit_Framework_TestCase');
+
+if (! $hasOldPhpUnit && $hasNewPhpUnit) {
+
+    class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase
+    {
+    }
+}
